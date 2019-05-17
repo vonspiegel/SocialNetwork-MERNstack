a29e496025af7fcf30ebdf3a1176f900ff50b45b
Pedro BM <p.bonilla.miralles@gmail.com>
2019-05-17 15:44:36 +0200

    new post created

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
 const auth = require('../../middleware/auth');
 
+const Post = require('../../models/Post');
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
+
 //@route  POST api/posts
 //@desc   Create a post
 //@access Private
@@ -20,6 +24,24 @@ router.post(
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
     }
   }
 );
