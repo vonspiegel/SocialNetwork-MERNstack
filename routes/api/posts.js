9bf391c2916fbdf309aff151c5964756b78cd98d
Pedro BM <p.bonilla.miralles@gmail.com>
2019-05-18 20:56:55 +0200

    delete post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -80,4 +80,32 @@ router.get('/:id', auth, async (req, res) => {
   }
 });
 
+//@route  DELETE api/posts/:id
+//@desc   Delete post
+//@access Private
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).send({ msg: 'Post not found' });
+    }
+
+    //Check user for not deleting from postman
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).send({ msg: 'User not authorized' });
+    }
+
+    await post.remove();
+
+    res.json({ msg: 'Post removed' });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).send({ msg: 'Post not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
