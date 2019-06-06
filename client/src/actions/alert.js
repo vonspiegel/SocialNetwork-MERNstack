1bdfcb538d15f603cafaf5f1e2894a78ba35438d
Pedro BM <p.bonilla.miralles@gmail.com>
2019-06-06 18:52:43 +0200

    alert action

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -3,4 +3,8 @@ import { SET_ALERT, REMOVE_ALERT } from './types';
 
 export const setAlert = (msg, alertType) => dispatch => {
   const id = uuid.v4(); //the version of uuid in order to get a random id
+  dispatch({
+    type: SET_ALERT,
+    payload: { msg, alertType, id }
+  });
 };
