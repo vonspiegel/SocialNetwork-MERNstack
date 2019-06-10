3959f0dd32aa8fa2e6e3ed709a4e4daf02ad976c
Pedro BM <p.bonilla.miralles@gmail.com>
2019-06-10 15:37:18 +0200

    loadUser into auth action

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -9,7 +9,24 @@ import {
 import setAuthToken from '../utils/setAuthToken';
 
 //Load user
-export const loadUser = () => async dispatch => {};
+export const loadUser = () => async dispatch => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+
+  try {
+    const res = await axios.get('/api/auth');
+
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: AUTH_ERROR
+    });
+  }
+};
 
 //Register user
 export const register = ({ name, email, password }) => async dispatch => {
