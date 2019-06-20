28888983386644a335f4d85a40912eb2e039e7e1
Pedro BM <p.bonilla.miralles@gmail.com>
2019-06-20 13:21:04 +0200

    dashboard button into Navbar

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -6,7 +6,7 @@ import { GET_PROFILE, PROFILE_ERROR } from './types';
 //Get current users profile
 export const getCurrentProfile = () => async dispatch => {
   try {
-    const res = await axios.get('api/profile/me');
+    const res = await axios.get('/api/profile/me');
 
     dispatch({
       type: GET_PROFILE,
