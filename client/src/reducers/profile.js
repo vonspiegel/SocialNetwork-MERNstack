d0a433afbcd43001106d7ca613069014a46e667c
Pedro BM <p.bonilla.miralles@gmail.com>
2019-06-29 13:42:28 +0200

    profile reducer evolved

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,9 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from '../actions/types';
+import {
+  GET_PROFILE,
+  PROFILE_ERROR,
+  CLEAR_PROFILE,
+  UPDATE_PROFILE
+} from '../actions/types';
 
 const initialState = {
   profile: null,
