993ced620c30d2a7f4edc661982148dc702d027d
Pedro BM <p.bonilla.miralles@gmail.com>
2019-06-30 14:56:41 +0200

    update profile into profile reducer

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -18,6 +18,7 @@ export default function(state = initialState, action) {
 
   switch (type) {
     case GET_PROFILE:
+    case UPDATE_PROFILE:
       return {
         ...state,
         profile: payload,
