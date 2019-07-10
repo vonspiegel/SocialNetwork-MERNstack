700106cf50d2e94fba9f30c4c214e2d33946fc36
Pedro BM <p.bonilla.miralles@gmail.com>
2019-07-10 23:36:47 +0100

    delete education

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -145,3 +145,22 @@ export const deleteExperience = id => async dispatch => {
     });
   }
 };
+
+//Delete education
+export const deleteEducation = id => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/profile/experience/${id}`);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Experience Removed', 'success'));
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
