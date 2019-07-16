dc899a84e00daacfd29bf95334f3ed51ebb2d329
Pedro BM <p.bonilla.miralles@gmail.com>
2019-07-16 13:28:44 +0200

    all posts from user removed

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,7 @@ const { check, validationResult } = require('express-validator/check');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 //@route  GET api/profile/me
 //@desc   Get current users profile
@@ -152,10 +153,10 @@ router.get('/user/:user_id', async (req, res) => {
 //@access Private
 router.delete('/', auth, async (req, res) => {
   try {
-    //@todo - remove users posts
-
+    //Remove user post
+    await Post.deleteMany({ user: req.user.id });
     //Remove profile
-    await Profile.findOneAndRemove({ user: req.user.id });
+    await Profile.findOneAndRemove({ user: req.user.id }); //The user is the logged in user
     // Remove user
     await User.findOneAndRemove({ _id: req.user.id });
     res.json({ msg: 'User deleted' });
