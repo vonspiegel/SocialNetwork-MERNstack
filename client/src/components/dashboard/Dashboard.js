2e1db2a1e4b51568de24e087720bda3b17d3f3f7
Pedro BM <p.bonilla.miralles@gmail.com>
2019-07-16 13:18:12 +0200

    deleteAccount button fixed

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -34,7 +34,7 @@ const Dashboard = ({
           <Education education={profile.education} />
 
           <div className='my-2'>
-            <button className='btn btn-danger' onclick={() => deleteAccount()}>
+            <button className='btn btn-danger' onClick={() => deleteAccount()}>
               <i className='fas fa-user-minus' /> Delete My Account
             </button>
           </div>
