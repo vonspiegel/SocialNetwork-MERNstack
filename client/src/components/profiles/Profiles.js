0edac37c757d4622f5632824fc672b51576584f6
Pedro BM <p.bonilla.miralles@gmail.com>
2019-07-22 21:57:35 +0200

    developer profiles added to Profiles

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -2,14 +2,36 @@ import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
-import { getProfiles } from '../../actions/profile';
+import ProfileItem from '../layout/Spinner';
+import { getProfiles } from '../profiles/ProfileItem';
 
-const Profiles = ({ getProfiles, profile: { profile, loading } }) => {
+const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
   useEffect(() => {
     getProfiles();
   }, [getProfiles]);
 
-  return <div />;
+  return (
+    <Fragment>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <h1 className='large text-primary'>Developers</h1>
+          <p className='lead'>
+            <i className='fab fa-connectdevelop' /> Browse and connect with
+            developers
+          </p>
+          <div className='profiles'>
+            {profiles.length > 0 ? (
+              <ProfileItem key={profile._id} profile={profile} />
+            ) : (
+              <h4>No profiles found...</h4>
+            )}
+          </div>
+        </Fragment>
+      )}
+    </Fragment>
+  );
 };
 
 Profiles.propTypes = {
