b94c7cd08209b85e1982b0f685f4c843a5df9ee6
Pedro BM <p.bonilla.miralles@gmail.com>
2019-08-01 19:36:06 +0200

    ProfileAbout into Profile

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -18,26 +18,38 @@ const ProfileTop = ({
       <p class='lead'>
         {status} {company && <span> at {company}</span>}
       </p>
-      <p>{location}</p>
+      <p>{location && <span>{location}</span>}</p>
       <div class='icons my-1'>
-        <a href='#' target='_blank' rel='noopener noreferrer'>
-          <i class='fas fa-globe fa-2x' />
-        </a>
-        <a href='#' target='_blank' rel='noopener noreferrer'>
-          <i class='fab fa-twitter fa-2x' />
-        </a>
-        <a href='#' target='_blank' rel='noopener noreferrer'>
-          <i class='fab fa-facebook fa-2x' />
-        </a>
-        <a href='#' target='_blank' rel='noopener noreferrer'>
-          <i class='fab fa-linkedin fa-2x' />
-        </a>
-        <a href='#' target='_blank' rel='noopener noreferrer'>
-          <i class='fab fa-youtube fa-2x' />
-        </a>
-        <a href='#' target='_blank' rel='noopener noreferrer'>
-          <i class='fab fa-instagram fa-2x' />
-        </a>
+        {website && (
+          <a href={website} target='_blank' rel='noopener noreferrer'>
+            <i class='fas fa-globe fa-2x' />
+          </a>
+        )}
+        {social && social.twitter && (
+          <a href={social.twitter} target='_blank' rel='noopener noreferrer'>
+            <i class='fab fa-twitter fa-2x' />
+          </a>
+        )}
+        {social && social.facebook && (
+          <a href={social.facebook} target='_blank' rel='noopener noreferrer'>
+            <i class='fab fa-facebook fa-2x' />
+          </a>
+        )}
+        {social && social.linkedin && (
+          <a href={social.linkedin} target='_blank' rel='noopener noreferrer'>
+            <i class='fab fa-linkedin fa-2x' />
+          </a>
+        )}
+        {social && social.youtube && (
+          <a href={social.youtube} target='_blank' rel='noopener noreferrer'>
+            <i class='fab fa-youtube fa-2x' />
+          </a>
+        )}
+        {social && social.instagram && (
+          <a href={social.instagram} target='_blank' rel='noopener noreferrer'>
+            <i class='fab fa-instagram fa-2x' />
+          </a>
+        )}
       </div>
     </div>
   );
