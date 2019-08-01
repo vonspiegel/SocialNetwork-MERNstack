a7a0e98160ec6d681f83c4ec8b8a7b1b2b30f431
Pedro BM <p.bonilla.miralles@gmail.com>
2019-08-01 19:53:22 +0200

    ProfileAbout with first name at bio

---
+++ client/src/components/profile/ProfileAbout.js
@@ -0,0 +1,44 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+const ProfileAbout = ({
+  profile: {
+    bio,
+    skills,
+    user: { name }
+  }
+}) => (
+  <div class='profile-about bg-light p-2'>
+    {bio && (
+      <Fragment>
+        <h2 class='text-primary'>{name.trim().split(' ')[0]}s Bio</h2>
+        <p>{bio}</p>
+        <div class='line' />
+      </Fragment>
+    )}
+    <h2 class='text-primary'>Skill Set</h2>
+    <div class='skills'>
+      <div class='p-1'>
+        <i class='fa fa-check' /> HTML
+      </div>
+      <div class='p-1'>
+        <i class='fa fa-check' /> CSS
+      </div>
+      <div class='p-1'>
+        <i class='fa fa-check' /> JavaScript
+      </div>
+      <div class='p-1'>
+        <i class='fa fa-check' /> Python
+      </div>
+      <div class='p-1'>
+        <i class='fa fa-check' /> C#
+      </div>
+    </div>
+  </div>
+);
+
+ProfileAbout.propTypes = {
+  profile: PropTypes.object.isRequired
+};
+
+export default ProfileAbout;
