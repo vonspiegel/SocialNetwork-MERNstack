2e44049c4f80129d43d9cd2aef2e52fa2b378334
Pedro BM <p.bonilla.miralles@gmail.com>
2019-08-03 13:59:32 +0200

    experience added to Profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -37,6 +37,10 @@ const Profile = ({
           <div className='profile-grid my-1'>
             <ProfileTop profile={profile} />
             <ProfileAbout profile={profile} />
+            <div className="profile-exp bg-white p-2">
+              <h2 className="text-primary">Experience</h2>
+              {profile.experience.length > 0 ? () : (<h4>No experience</h4>)}
+            </div>
           </div>
         </Fragment>
       )}
