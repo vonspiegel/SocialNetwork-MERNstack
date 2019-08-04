5969056d7b6b86f44e537b03814c6f639987c691
Pedro BM <p.bonilla.miralles@gmail.com>
2019-08-04 15:43:25 +0200

    ProfileGithub created

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -2,18 +2,27 @@ import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux'; //since we're calling actions
 import Spinner from '../layout/Spinner';
-import { get}
+import { getGithubRepos } from '../../actions/profile';
 
-const ProfileGithub = props => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const ProfileGithub = ({ username, getGithubRepos, repos }) => {
+  useEffect(() => {
+    getGithubRepos(username);
+  }, [getGithubRepos, username]);
+
+  return <div />;
+};
 
 ProfileGithub.propTypes = {
+  getGithubRepos: PropTypes.func.isRequired,
+  repos: PropTypes.array.isRequired,
+  username: PropTypes.string.isRequired
+};
 
-}
+const mapStateToProps = state => ({
+  repos: state.profile.repos
+});
 
-export default ProfileGithub
+export default connect(
+  mapStateToProps,
+  { getGithubRepos }
+)(ProfileGithub);
