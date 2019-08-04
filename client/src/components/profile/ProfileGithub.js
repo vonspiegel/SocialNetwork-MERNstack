049ac241efd9408cb5f919ff2db18bcd04feb193
Pedro BM <p.bonilla.miralles@gmail.com>
2019-08-04 14:20:18 +0200

    ProfileGithub with props

---
+++ client/src/components/profile/ProfileGithub.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux'; //since we're calling actions
+import Spinner from '../layout/Spinner';
+import { get}
+
+const ProfileGithub = props => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+ProfileGithub.propTypes = {
+
+}
+
+export default ProfileGithub
