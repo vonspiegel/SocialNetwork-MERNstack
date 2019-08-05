3800d0358b681df1f71dd88eadfde52d2550e089
Pedro BM <p.bonilla.miralles@gmail.com>
2019-08-05 14:36:11 +0200

    Navbar fixed

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -11,6 +11,9 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
         <Link to='/profiles'>Developers</Link>
       </li>
       <li>
+        <Link to='/posts'>Posts</Link>
+      </li>
+      <li>
         <Link to='/dashboard'>
           <i className='fas fa-user' />{' '}
           <span className='hide-sm'>Dashboard</span>
@@ -29,9 +32,6 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     <ul>
       <li>
         <Link to='/profiles'>Developers</Link>
-      </li>
-      <li>
-        <Link to='/posts'>Posts</Link>
       </li>
       <li>
         <Link to='/register'>Register</Link>
