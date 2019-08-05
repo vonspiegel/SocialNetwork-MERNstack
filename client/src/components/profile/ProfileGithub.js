93f4002828cea66aa8346a9af6ec679e3b8e6491
Pedro BM <p.bonilla.miralles@gmail.com>
2019-08-05 12:34:37 +0200

    ProfileGithub with repos

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -9,7 +9,30 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
     getGithubRepos(username);
   }, [getGithubRepos, username]);
 
-  return <div />;
+  return (
+    <div className='profile-github'>
+      <h2 className='text-primary my-1'>Github Repos</h2>
+      {repos === null ? (
+        <Spinner />
+      ) : (
+        repos.map(repo => (
+          <div key={repo._id} className='repo bg-white p-1 my-1'>
+            <div>
+              <h4>
+                <a
+                  href={repo.html_url}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  {repo.name}
+                </a>
+              </h4>
+            </div>
+          </div>
+        ))
+      )}
+    </div>
+  );
 };
 
 ProfileGithub.propTypes = {
