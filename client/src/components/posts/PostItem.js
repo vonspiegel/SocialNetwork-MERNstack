a770f9c58d1916126c6cfe2aff252db64779068c
Pedro BM <p.bonilla.miralles@gmail.com>
2019-08-06 16:12:36 +0200

    PostItem with comments

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -11,35 +11,33 @@ const PostItem = ({
   <div class='post bg-white p-1 my-1'>
     <div>
       <a href='profile.html'>
-        <img
-          class='round-img'
-          src='https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200'
-          alt=''
-        />
-        <h4>John Doe</h4>
+        <img class='round-img' src={avatar} alt='' />
+        <h4>{name}</h4>
       </a>
     </div>
     <div>
-      <p class='my-1'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint possimus
-        corporis sunt necessitatibus! Minus nesciunt soluta suscipit nobis. Amet
-        accusamus distinctio cupiditate blanditiis dolor? Illo perferendis
-        eveniet cum cupiditate aliquam?
+      <p class='my-1'>{text}</p>
+      <p class='post-date'>
+        Posted on <Moment format='DD/MM/YYYY'>{date}</Moment>
       </p>
-      <p class='post-date'>Posted on 04/16/2019</p>
       <button type='button' class='btn btn-light'>
         <i class='fas fa-thumbs-up' />
-        <span>4</span>
+        <span>{likes.length}</span>
       </button>
       <button type='button' class='btn btn-light'>
         <i class='fas fa-thumbs-down' />
       </button>
-      <a href='post.html' class='btn btn-primary'>
-        Discussion <span class='comment-count'>2</span>
-      </a>
-      <button type='button' class='btn btn-danger'>
-        <i class='fas fa-times' />
-      </button>
+      <Link to={`/post/${_id}`} class='btn btn-primary'>
+        Discussion{' '}
+        {comments.length > 0 && (
+          <span class='comment-count'>{comments.length}</span>
+        )}
+      </Link>
+      {!auth.loading && user === auth.user._id && (
+        <button type='button' class='btn btn-danger'>
+          <i class='fas fa-times' />
+        </button>
+      )}
     </div>
   </div>
 );
