6883581764c3c24dff0694e2d5e1620f1e6cac2c
Pedro BM <p.bonilla.miralles@gmail.com>
2019-08-08 14:05:28 +0200

    post added at the top of the list

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -62,7 +62,7 @@ export const removeLike = postId => async dispatch => {
 //Delete post
 export const deletePost = postId => async dispatch => {
   try {
-    const res = await axios.delete(`/api/posts/${postId}`);
+    await axios.delete(`/api/posts/${postId}`);
 
     dispatch({
       type: DELETE_POST,
