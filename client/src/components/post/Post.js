5bab5b7b6382b4bb99a4dc56d70e3a8526458aa1
Pedro BM <p.bonilla.miralles@gmail.com>
2019-08-09 14:51:47 +0200

    PostItem added to Post

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import PostItem from '../posts/PostItem';
 import { getPost } from '../../actions/post';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
@@ -10,7 +11,13 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
     getPost(match.params.id);
   }, [getPost, match.params.id]);
 
-  return <div>post</div>;
+  return loading || post === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <PostItem post={post} showActions={false} />
+    </Fragment>
+  );
 };
 
 Post.propTypes = {
